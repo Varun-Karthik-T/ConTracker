a809574406e5ad7a071d61fd2da710fd7336c71a
Varun-Karthik-T <varunkarthikceg@gmail.com>
2025-01-11 02:22:39 +0530

    project dapp done

--- dapp/contracts/approvedContract.sol
+++ dapp/contracts/approvedContract.sol
@@ -1,10 +1,10 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 contract ApprovedContract {
     struct Winner {
-        uint256 issue_id;
-        uint256 contractor_id;
+        uint256 issueId;
+        uint256 contractorId;
+        uint256 projectId;
         string name;
         uint256 tenure;
         uint256 amount;
@@ -18,8 +18,9 @@ contract ApprovedContract {
 
     // Event to announce the winning contractor
     event WinnerDeclared(
-        uint256 issue_id,
-        uint256 contractor_id,
+        uint256 issueId,
+        uint256 contractorId,
+        uint256 projectId,
         string name,
         uint256 tenure,
         uint256 amount,
@@ -28,47 +29,38 @@ contract ApprovedContract {
         string approvedBy
     );
 
+    modifier onlyOnce() {
+        require(!winnerSet, "Winner is already set!");
+        _;
+    }
+
     function setWinner(
-        uint256 _issue_id,
-        uint256 _contractor_id,
+        uint256 _issueId,
+        uint256 _contractorId,
+        uint256 _projectId,
         string memory _name,
         uint256 _tenure,
         uint256 _amount,
         string memory _location,
         string memory _verifiedBy,
         string memory _approvedBy
-    ) public {
-        require(!winnerSet, "Winner is already set!");
-        winner = Winner(_issue_id, _contractor_id, _name, _tenure, _amount, _location, _verifiedBy, _approvedBy);
+    ) public onlyOnce {
+        winner = Winner({
+            issueId: _issueId,
+            contractorId: _contractorId,
+            projectId: _projectId,
+            name: _name,
+            tenure: _tenure,
+            amount: _amount,
+            location: _location,
+            verifiedBy: _verifiedBy,
+            approvedBy: _approvedBy
+        });
         winnerSet = true;
-
-        emit WinnerDeclared(_issue_id, _contractor_id, _name, _tenure, _amount, _location, _verifiedBy, _approvedBy);
+        emit WinnerDeclared(_issueId, _contractorId, _projectId, _name, _tenure, _amount, _location, _verifiedBy, _approvedBy);
     }
 
-    function getWinner()
-        public
-        view
-        returns (
-            uint256,
-            uint256,
-            string memory,
-            uint256,
-            uint256,
-            string memory,
-            string memory,
-            string memory
-        )
-    {
-        require(winnerSet, "Winner is not set yet!");
-        return (
-            winner.issue_id,
-            winner.contractor_id,
-            winner.name,
-            winner.tenure,
-            winner.amount,
-            winner.location,
-            winner.verifiedBy,
-            winner.approvedBy
-        );
+    function getWinner() public view returns (Winner memory) {
+        return winner;
     }
 }
--- dapp/test/testBidding.js
+++ dapp/test/testBidding.js
@@ -1,106 +1,10 @@
-// const ApprovedContract = artifacts.require("ApprovedContract");
-
-// function getWinnerDetails() {
-//   return {
-//     issueId: 1,
-//     contractorId: 101,
-//     contractorName: "John Doe",
-//     tenure: 365,
-//     amount: 1000000,
-//     location: "New York, USA",
-//     verifiedBy: "Verifier",
-//     approvedBy: "Approver"
-//   };
-// }
-
-// contract("ApprovedContract", (accounts) => {
-//   let biddingInstance;
-//   const [owner, verifiedBy, approvedBy] = accounts;
-
-//   beforeEach(async () => {
-//     biddingInstance = await ApprovedContract.new();
-//   });
-
-//   it("should deploy the contract", async () => {
-//     const address = await biddingInstance.address;
-//     assert.notEqual(address, 0x0, "Contract was not deployed successfully");
-//   });
-
-//   it("should set and retrieve the winner details correctly", async () => {
-//     const {
-//       issueId,
-//       contractorId,
-//       contractorName,
-//       tenure,
-//       amount,
-//       location,
-//       verifiedBy,
-//       approvedBy
-//     } = getWinnerDetails();
-
-//     await biddingInstance.setWinner(issueId, contractorId, contractorName, tenure, amount, location, verifiedBy, approvedBy, { from: owner });
-
-//     const winner = await biddingInstance.getWinner();
-
-//     // Display the fetched data
-//     console.log("Issue ID:", winner[0].toString());
-//     console.log("Contractor ID:", winner[1].toString());
-//     console.log("Winner Name:", winner[2]);
-//     console.log("Winner Tenure:", winner[3].toString());
-//     console.log("Winner Amount:", winner[4].toString());
-//     console.log("Winner Location:", winner[5]);
-//     console.log("Verified By:", winner[6]);
-//     console.log("Approved By:", winner[7]);
-
-//     assert.equal(winner[0].toString(), issueId.toString(), "Issue ID does not match");
-//     assert.equal(winner[1].toString(), contractorId.toString(), "Contractor ID does not match");
-//     assert.equal(winner[2], contractorName, "Winner name does not match");
-//     assert.equal(winner[3].toString(), tenure.toString(), "Winner tenure does not match");
-//     assert.equal(winner[4].toString(), amount.toString(), "Winner amount does not match");
-//     assert.equal(winner[5], location, "Winner location does not match");
-//     assert.equal(winner[6], verifiedBy, "Verified By does not match");
-//     assert.equal(winner[7], approvedBy, "Approved By does not match");
-//   });
-
-//   it("should not allow setting the winner twice", async () => {
-//     const {
-//       issueId,
-//       contractorId,
-//       contractorName,
-//       tenure,
-//       amount,
-//       location,
-//       verifiedBy,
-//       approvedBy
-//     } = getWinnerDetails();
-
-//     await biddingInstance.setWinner(issueId, contractorId, contractorName, tenure, amount, location, verifiedBy, approvedBy, { from: owner });
-
-//     try {
-//       await biddingInstance.setWinner(
-//         2,
-//         102,
-//         "Another Contractor",
-//         60,
-//         300000,
-//         "Texas",
-//         "Verifier",
-//         "Approver"
-//       );
-//       assert.fail("The contract allowed setting the winner twice!");
-//     } catch (error) {
-//       assert.include(error.message, "Winner is already set!", "Expected error message not received");
-//     }
-//   });
-// });
-
-
 const ApprovedContract = artifacts.require("ApprovedContract");
 
 function getWinnerDetails() {
   return {
     issueId: 1,
     contractorId: 101,
+    projectId: 4,
     contractorName: "Jane Doe",
     tenure: 365,
     amount: 1000000,
@@ -110,91 +14,67 @@ function getWinnerDetails() {
   };
 }
 
-function runTests() {
-  contract("ApprovedContract", (accounts) => {
-    let biddingInstance;
-    const [owner, verifiedBy, approvedBy] = accounts;
+contract("ApprovedContract", (accounts) => {
+  let biddingInstance;
+  const [owner, verifiedBy, approvedBy] = accounts;
 
-    beforeEach(async () => {
-      biddingInstance = await ApprovedContract.new();
-    });
-
-    it("should deploy the contract", async () => {
-      const address = await biddingInstance.address;
-      assert.notEqual(address, 0x0, "Contract was not deployed successfully");
-    });
-
-    it("should set and retrieve the winner details correctly", async () => {
-      const {
-        issueId,
-        contractorId,
-        contractorName,
-        tenure,
-        amount,
-        location,
-        verifiedBy,
-        approvedBy
-      } = getWinnerDetails();
-
-      await biddingInstance.setWinner(issueId, contractorId, contractorName, tenure, amount, location, verifiedBy, approvedBy, { from: owner });
-
-      const winner = await biddingInstance.getWinner();
-
-      // Display the fetched data
-      console.log("Issue ID:", winner[0].toString());
-      console.log("Contractor ID:", winner[1].toString());
-      console.log("Winner Name:", winner[2]);
-      console.log("Winner Tenure:", winner[3].toString());
-      console.log("Winner Amount:", winner[4].toString());
-      console.log("Winner Location:", winner[5]);
-      console.log("Verified By:", winner[6]);
-      console.log("Approved By:", winner[7]);
-
-      assert.equal(winner[0].toString(), issueId.toString(), "Issue ID does not match");
-      assert.equal(winner[1].toString(), contractorId.toString(), "Contractor ID does not match");
-      assert.equal(winner[2], contractorName, "Winner name does not match");
-      assert.equal(winner[3].toString(), tenure.toString(), "Winner tenure does not match");
-      assert.equal(winner[4].toString(), amount.toString(), "Winner amount does not match");
-      assert.equal(winner[5], location, "Winner location does not match");
-      assert.equal(winner[6], verifiedBy, "Verified By does not match");
-      assert.equal(winner[7], approvedBy, "Approved By does not match");
-    });
-
-    it("should not allow setting the winner twice", async () => {
-      const {
-        issueId,
-        contractorId,
-        contractorName,
-        tenure,
-        amount,
-        location,
-        verifiedBy,
-        approvedBy
-      } = getWinnerDetails();
-
-      await biddingInstance.setWinner(issueId, contractorId, contractorName, tenure, amount, location, verifiedBy, approvedBy, { from: owner });
-
-      try {
-        await biddingInstance.setWinner(
-          2,
-          102,
-          "Another Contractor",
-          60,
-          300000,
-          "Texas",
-          "Verifier",
-          "Approver"
-        );
-        assert.fail("The contract allowed setting the winner twice!");
-      } catch (error) {
-        assert.include(error.message, "Winner is already set!", "Expected error message not received");
-      }
-    });
+  beforeEach(async () => {
+    biddingInstance = await ApprovedContract.new();
   });
-}
 
-module.exports = {
-  runTests
-};
+  it("should deploy the contract", async () => {
+    const address = await biddingInstance.address;
+    assert.notEqual(address, 0x0, "Contract was not deployed successfully");
+  });
 
-runTests();
+  it("should set and retrieve the winner details correctly", async () => {
+    const {
+      issueId,
+      contractorId,
+      projectId,
+      contractorName,
+      tenure,
+      amount,
+      location,
+      verifiedBy,
+      approvedBy
+    } = getWinnerDetails();
+
+    await biddingInstance.setWinner(issueId, contractorId, projectId, contractorName, tenure, amount, location, verifiedBy, approvedBy, { from: owner });
+
+    const winner = await biddingInstance.getWinner();
+
+    assert.equal(winner.issueId, issueId, "Issue ID does not match");
+    assert.equal(winner.contractorId, contractorId, "Contractor ID does not match");
+    assert.equal(winner.projectId, projectId, "Project ID does not match");
+    assert.equal(winner.name, contractorName, "Contractor name does not match");
+    assert.equal(winner.tenure, tenure, "Tenure does not match");
+    assert.equal(winner.amount, amount, "Amount does not match");
+    assert.equal(winner.location, location, "Location does not match");
+    assert.equal(winner.verifiedBy, verifiedBy, "Verified by does not match");
+    assert.equal(winner.approvedBy, approvedBy, "Approved by does not match");
+  });
+
+  it("should not allow setting the winner twice", async () => {
+    const {
+      issueId,
+      contractorId,
+      projectId,
+      contractorName,
+      tenure,
+      amount,
+      location,
+      verifiedBy,
+      approvedBy
+    } = getWinnerDetails();
+
+    await biddingInstance.setWinner(issueId, contractorId, projectId, contractorName, tenure, amount, location, verifiedBy, approvedBy, { from: owner });
+
+    try {
+      await biddingInstance.setWinner(issueId, contractorId, projectId, contractorName, tenure, amount, location, verifiedBy, approvedBy, { from: owner });
+      assert.fail("Should not allow setting the winner twice");
+    } catch (error) {
+      assert(error.message.includes("Winner is already set!"), "Expected winner already set error");
+    }
+  });
+});
