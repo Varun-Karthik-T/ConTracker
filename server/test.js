e86de9feb3c9043a03b904d1a764d27f6f8b32bf
Varun-Karthik-T <varunkarthikceg@gmail.com>
2025-01-11 11:35:42 +0530

    .

--- server/test.js
+++ server/test.js
@@ -3,12 +3,12 @@ const Web3 = require('web3');
 require('dotenv').config();
 
 const { RPC_URL } = process.env;
-const web3 = new Web3(RPC_URL);
+const web3 = new Web3(RPC_URL); 
 
 (async () => {
   try {
-    await Project.addWinner(2, 102, 5, 'Jfdfe Doe', 365, 1000000, 'New York, USA', 'Verifier', 'Approver');
-    await Project.getWinnerDetails(5);
+    await Project.addWinner(3, 102, 40, 'Highway 278', 600, 2000000, 'Coimbatore', 'Varun', 'Karthik');
+    await Project.getWinnerDetails(40);
   } catch (error) {
     console.error('Error:', error);
   } finally {
--- server/vote.js
+++ server/vote.js
@@ -5,26 +5,24 @@ const { ACCOUNT_ADDRESS } = process.env;
 
 async function main() {
   const spendDetails = {
-    spendId: 5, // Ensure this is the correct spendId you want to use (sequential)
-    reason: "Contractor spend for road repar"
+    spendId: 40,
+    reason: "Initial raw materials"
   };
 
-  const account = ACCOUNT_ADDRESS; // Replace with your account address
-
+  const account = ACCOUNT_ADDRESS;
   // Create a spend
   const createdSpend = await createSpend(spendDetails, account);
   console.log("Created Spend:", createdSpend);
 
-  // Voting parameters
+
   const approvalVotes = 60;
   const denialVotes = 40;
   const totalVotes = 100;
 
-  // Vote on the spend
   const votedSpend = await voteOnSpend(createdSpend.spendId, approvalVotes, denialVotes, totalVotes, account);
   console.log("Voted Spend:", votedSpend);
 
-  // Fetch the voted spend
+
   const fetchedSpend = await fetchSpend(createdSpend.spendId);
   console.log("Fetched Spend:", fetchedSpend);
 }
