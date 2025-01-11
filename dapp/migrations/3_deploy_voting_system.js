0bc2534586c8941c21398409cc9257a8c5844c84
Varun-Karthik-T <varunkarthikceg@gmail.com>
2025-01-11 10:19:44 +0530

    dapp update

--- dapp/migrations/3_deploy_voting_system.js
+++ dapp/migrations/3_deploy_voting_system.js
@@ -1,5 +1,8 @@
 const VotingSystem = artifacts.require("VotingSystem");
 
 module.exports = function (deployer) {
-  deployer.deploy(VotingSystem);
+  deployer.deploy(VotingSystem, {
+    gas: 2000000, // Lower gas limit
+    gasPrice: 1000000000 // Lower gas price (1 gwei)
+  });
 };
